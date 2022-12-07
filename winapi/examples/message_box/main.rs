ea5703aee142d2c2dd6a3ec5e350aade0b4a2ddf
Aza Maulana <azamaulanaaa@gmail.com>
2022-12-07 21:04:21 +0700

    feat(winapi): add message box example

---
+++ winapi/examples/message_box/main.rs
@@ -0,0 +1,11 @@
+use std::ptr::null_mut;
+use winapi::um::winuser;
+
+fn main() {
+    let title = b"Horray!!!\0";
+    let body = b"Hello, world!\0";
+
+    unsafe {
+        winuser::MessageBoxA(null_mut(), body.as_ptr().cast(), title.as_ptr().cast(), 0);
+    }
+}
